863567b23ca7fd19d6a694181cc3f6d673ce0ecc
sameer gul <sameergul12345@gmail.com>
2025-02-22 18:28:57 +0500

    Update render.yaml to be more explicit about WSGI application

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-cd /opt/render/project/src
-export PYTHONPATH=/opt/render/project/src
-exec gunicorn gull_autos.wsgi:application --bind 0.0.0.0:$PORT --log-file -
