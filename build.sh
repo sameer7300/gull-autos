924bbbb3a8eee29ea2d935835d39206748ff6549
sameer gul <sameergul12345@gmail.com>
2025-02-22 17:44:16 +0500

    Fix environment configuration using .env.prod

--- build.sh
+++ build.sh
@@ -5,11 +5,8 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Create .env file
-echo "DEBUG=False" > .env
-echo "ALLOWED_HOSTS=.onrender.com" >> .env
-echo "DJANGO_SETTINGS_MODULE=gull_autos.settings" >> .env
-echo "PYTHONPATH=/opt/render/project/src" >> .env
+# Copy production env file
+cp .env.prod .env
 
 # Make sure the project directory is in PYTHONPATH
 export PYTHONPATH=/opt/render/project/src:$PYTHONPATH
